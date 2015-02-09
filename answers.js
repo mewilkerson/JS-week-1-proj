e4cb4f4f14ab34d1d02832b9686a77ed17e19085
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-02-08 23:56:47 -0500

    What (little) I've been able to get done

--- answers.js
+++ answers.js
@@ -1,3 +1,37 @@
-// Some Text
+// Problem # 1
+// Need to 1) locate price for every item -- use filter method?, 2) add all the prices together and divide by total number of prices, 3) console.log the answer
+var average = items.filter (function(itemPrice){
+  return items.price;
+}
 
-// Some more Text
+  );
+
+
+// Problem #2
+// 
+
+
+
+// Problem # 4
+// 
+
+var material = ["plastic", "metal", "glass", "cloth"];
+
+  var hasWood = function(materials) {
+
+      var containsWood = false;
+
+material.forEach(function(currentMaterial){ 
+  if(currentMaterial === "wood"; {}
+    containsWood = true;
+      }
+    });
+    return containsWood;
+  }
+
+  var itemsWithWood = items.filter(function(currentItem){
+  var currentMaterials = currentItem.materials;
+  var containsWood = hasWood(currentMaterials);
+  return containsWood;
+
+});
